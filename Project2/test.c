54758515c782abd35c16c034509e41e69b75efd0
Eden <1023178387@qq.com>
2019-06-01 17:31:58 +0800

    声明常变量，

--- Project2/test.c
+++ Project2/test.c
@@ -2,45 +2,19 @@
 
 void main(void)
 {
-	short sh1 = 1;
-	int i1 = 1;
-	long l1 = 65538;
-	void* i1_adders = &i1;
+	void *p;
+	//const int a = 33;
 
-	char c1 = 255;
-	void* c1_adders = &c1;
+	const int a;//声明常变量
 
-	float  f1 = 3.0e9f;
-	void* f1_adders = &f1;
-	double d1 = 33;
-	long double ld1 = 33;
 
-	enum jar_type {no,yes,null} enu1,enu2;//枚举变量中的枚举元素，系统是依照常量来处理的
 
-	printf("整型数据的长度\r\n");
-	printf("short       = %d\r\n", sizeof(short));
-	printf("int         = %d\r\n", sizeof(int));
-	printf("long        = %d\r\n", sizeof(long));
-	printf("long long   = %d\r\n", sizeof(long long));
-	printf("i1          = %x\r\n", i1);
-	printf("sh1         = %x\r\n", sh1);
-	printf("l1          = %x\r\n", l1);
-	sh1 = (short)l1;
-	printf("sh1         = %x\r\n", sh1);
-	printf("i1_adders         = %p\r\n", i1_adders);
+	printf("a = %d\r\n",a);
+	p = &a;
+	printf("a_adders = %p\r\n", p);
 
-	printf("\r\n字符型数据的长度\r\n");
-	printf("char        = %d\r\n", sizeof(char));
-	printf("c           = %d\r\n", c1);
-	printf("c1_adders         = %p\r\n", c1_adders);
 
-	printf("\r\n浮点型数据的长度\r\n");
-	printf("float       = %d\r\n", sizeof(float));
-	printf("double      = %d\r\n", sizeof(double));
-	printf("long double = %d\r\n", sizeof(long double));
-	printf("f1          = %f\r\n", f1);
-	printf("f1_adders         = %p\r\n", f1_adders);
-	printf("d1          = %f\r\n", d1);
+
 
 
 
